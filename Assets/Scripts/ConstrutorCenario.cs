c47d94a448fd28ad6044a2c73f18f2536cc210de
Rodrigo Pastorini <rodrigopastorfelis@gmail.com>
2022-03-03 12:10:16 -0300

    Meu projeto de aprendizado

---
+++ Assets/Scripts/ConstrutorCenario.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConstrutorCenario: MonoBehaviour
+{
+    public GameObject construtor;
+    public PosCubo cubo;
+    public int largura, altura;
+    [HideInInspector]
+    public float x, y;
+    public Vector3 central;
+    [HideInInspector]
+    public int item;
+    private void Awake()
+    {
+        item=largura * altura / 2;
+        ConstroiMapa(largura,altura,cubo,construtor);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public  void  ConstroiMapa(int largura, int altura, PosCubo cubo,GameObject construtor)
+    {
+        int contador = 0;
+        Vector3 posMapa = new Vector3(-1, -1, 0);
+            int[,] mapa = new int[largura, altura];
+            for (int i = 0; i <= largura; i++)
+            {
+                posMapa = new Vector3(posMapa.x + 1, 0, 0);
+                for (int j = 0; j < altura; j++)
+                {    
+                    posMapa = new Vector3(posMapa.x, posMapa.y + 1, 0);
+                    x = posMapa.x;
+                    y = posMapa.y;
+                    Instantiate(cubo, posMapa, transform.rotation,construtor.transform);
+                    if (item == contador)
+                    {
+                        central = posMapa;
+                    }
+
+                    contador++;
+                }
+            }
+    }
+}
+
---
+++ Assets/Scripts/ControladorJogo.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class ControladorJogo : MonoBehaviour
+{
+    public int nBarcos;
+    public int barcos = 0;
+    private ConstrutorCenario _meuConstrutor;
+    public Camera cameraPrincipal;
+    private float _sizeCamera;
+    private void Awake()
+    {
+        _meuConstrutor = GameObject.FindWithTag("Construtor").GetComponent<ConstrutorCenario>();
+    }
+
+    void Start()
+    {
+        cameraPrincipal.orthographicSize = (_meuConstrutor.altura + _meuConstrutor.largura) / 2;
+        cameraPrincipal.transform.position = new Vector3(_meuConstrutor.central.x,(_meuConstrutor.altura + _meuConstrutor.largura) / 4 , -_meuConstrutor.item/0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
